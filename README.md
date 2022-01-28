5bfcdc4e39237d534693637187e10d22ae5a9d4c
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-28 03:14:41 -0300

    refactor: request compliment controller

---
+++ README.md
@@ -0,0 +1,23 @@
+## Projeto
+
+Valoriza é uma plataforma para promover o reconhecimento entre companheiros de equipe.
+
+## Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- Node.js;
+- Typescript;
+- Express;
+- JSONWebToken;
+- Docker;
+- Celebrate;
+
+## Como executar
+
+- Clone o repositório.
+- Rode `yarn` para baixar as dependências.
+- Rode `yarn typeorm migration:run` para criar as tabelas do banco de dados.
+- Rode o `yarn` dev para iniciar a aplicação.
+
+Por fim, a aplicação estará disponível em http://localhost:3000
--- src/modules/compliments/infra/http/controllers/CreateComplimentController.ts
+++ src/modules/compliments/infra/http/controllers/CreateComplimentController.ts
@@ -4,13 +4,14 @@ import { container } from 'tsyringe';
 
 class CreateComplimentController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { message, user_sender, user_receiver, tag_id } = request.body;
+    const { message, user_receiver, tag_id } = request.body;
+    const { user_id } = request;
 
     const createComplimentService = container.resolve(CreateComplimentService);
 
     const compliment = await createComplimentService.execute({
       message,
-      user_sender,
+      user_sender: user_id,
       user_receiver,
       tag_id,
     });
